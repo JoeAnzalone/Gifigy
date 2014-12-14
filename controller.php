3aebdfe24672b5ca8ed15306cea95fde4430644f
Joe Anzalone <Joe@Shmit.com>
2014-12-14 09:43:36 -0500

    Randomize placeholder usernames

---
+++ controller.php
@@ -0,0 +1,33 @@
+<?php
+
+$placeholder_usernames = array(
+    '16-bitch',
+    'anamanaguchi',
+    'auduno',
+    'beeandpuppycat',
+    'beeple',
+    'beesandbombs',
+    'cindysuen',
+    'coinfarts',
+    'ernie',
+    'gnumblr',
+    'greatartistssteal',
+    'humansofnewyork',
+    'jasonclarke',
+    'kengoisms',
+    'literallysame',
+    'mensweardog',
+    'nightbagel',
+    'pixelatedcrown',
+    'pixeljamgames',
+    'shakeshack',
+    'shellmnop',
+    'skillrex',
+    'taylorswift',
+    'wwwtxt',
+    'zolloc',
+);
+
+$random_placeholder_usernames = $placeholder_usernames;
+shuffle($random_placeholder_usernames);
+$random_placeholder_usernames = array_slice($random_placeholder_usernames, 0, 3);
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once 'controller.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,7 +13,9 @@
         <canvas id="inputCanvas" width="320" height="240" style="display:none"></canvas>
         <div class="submit-form-wrapper">
             <form class="submit-form" style="display: none;">
-                    <input type="text" class="usernames" name="usernames" placeholder="htmlbyjoe, kcreate, david">
+                    <label>Who are you people?
+                        <input type="text" class="usernames" name="usernames" placeholder="<?php echo implode(', ', $random_placeholder_usernames); ?>">
+                    </label>
                     <input type="hidden" class="photo-data" name="photo-data">
                     <button type="button" class="discard">Discard</button>
                     <button type="submit">Post</button>
